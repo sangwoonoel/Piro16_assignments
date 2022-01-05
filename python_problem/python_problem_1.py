588043f3f5869d3ea68ee9cd741d96e88b20cc9e
Sangwoo Shin <sangwoonoel@naver.com>
2022-01-05 12:19:52 +0900

    3단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,2 +1,17 @@
+class NotOneTwoThreeError(Exception):
+    def __init__(self):
+        super().__init__('1,2,3 중 하나를 입력하세요')
+
 num = 0
-n = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+
+while True:
+ try:
+  num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+  if num > 3 or num < 1:
+   raise NotOneTwoThreeError
+ except ValueError:
+  print('정수를 입력하세요')
+ except NotOneTwoThreeError as e:
+  print(e)
+ else:
+  break
