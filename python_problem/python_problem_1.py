7a9b5e21c4003bdc9fe2e45e32f6524c342e8e4f
Sangwoo Shin <sangwoonoel@naver.com>
2022-01-05 23:24:50 +0900

    9단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,3 +1,5 @@
+from random import randint
+
 class NotOneTwoThreeError(Exception):
       def __init__(self):
             super().__init__('1,2,3 중 하나를 입력하세요')
@@ -6,29 +8,33 @@ game = 0 #게임 숫자를 저장하는 변수
 turn = 0 #누구의 턴인지 저장하는 변수
 
 def brgame():
+      if turn % 2 != 0: 
+            num = randint(1,3)
+      else:
+            num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
+            if num > 3 or num < 1:
+                  raise NotOneTwoThreeError
       for i in range(num):
             global game
             game += 1            
                         
             if turn % 2 != 0:             
-                  print('playerA : ', game)
+                  print('computer : ', game)
                   if game == 31:
-                        print('playerB win!')
+                        print('player win!')
                         return 0 
 
             else:
-                  print('playerB : ', game)
+                  print('player : ', game)
                   if game == 31:
-                        print('playerA win!')
+                        print('computer win!')
                         return 0
 
 while True:                    
       turn += 1
       while True:
             try:
-              num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
-              if num > 3 or num < 1:
-                    raise NotOneTwoThreeError
+                  end = brgame()
             except ValueError:
               print('정수를 입력하세요')
             except NotOneTwoThreeError as e:
@@ -36,7 +42,7 @@ while True:
             else:
                   break
 
-      end = brgame()
+      
       if end == 0:
             break
 
