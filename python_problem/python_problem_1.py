9ad87dec6d932c8567672637cbf4dd0423b87868
Sangwoo Shin <sangwoonoel@naver.com>
2022-01-05 13:35:01 +0900

    6단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,31 +1,45 @@
 class NotOneTwoThreeError(Exception):
-     def __init__(self):
-           super().__init__('1,2,3 중 하나를 입력하세요')
-
+      def __init__(self):
+            super().__init__('1,2,3 중 하나를 입력하세요')
 num = 0
-game = 0 #게임 숫자를 세주는 변수
-turn = 0 #누구의 턴인지 알려주는 변수
+game = 0 #게임 숫자를 저장하는 변수
+turn = 0 #누구의 턴인지 저장하는 변수
 
 while True:
- turn += 1
- while True:
-  try:
-   num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
-   if num > 3 or num < 1:
-    raise NotOneTwoThreeError
-  except ValueError:
-   print('정수를 입력하세요')
-  except NotOneTwoThreeError as e:
-   print(e)
-  else:
-   break
- if turn % 2 != 0:
-  for i in range(num):
-   game += 1
-   print('PlayerA :', game)
- else:
-  for i in range(num):
-   game += 1
-   print('PlayerB :', game)
+      if game == 31:
+            break
+                        
+      turn += 1
+      while True:
+            try:
+              num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
+              if num > 3 or num < 1:
+                    raise NotOneTwoThreeError
+            except ValueError:
+              print('정수를 입력하세요')
+            except NotOneTwoThreeError as e:
+              print(e)
+            else:
+                  break
+
+      if turn % 2 != 0:
+            for i in range(num):
+                  game += 1
+                  if game == 31:
+                        break
+                        
+                  print('playerA : ', game)
+      else:
+            for i in range(num):
+                  game += 1
+                  if game == 31:
+                      break
+                        
+                  print('playerB : ', game)
+    
+          
+                
+            
+
 
 
